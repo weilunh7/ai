6388ed83e5c86e8cf751f35eb299dea078a35a82
weilunh7 <99935037+weilunh7@users.noreply.github.com>
2024-03-07 11:38:31 +0800

    Update TSP_climb_me.py

--- TSP_climb_me.py
+++ TSP_climb_me.py
@@ -1,4 +1,7 @@
-#本程式參考https://blog.csdn.net/zw17302560727/article/details/122304577和https://github.com/ccc112b/py2cs/blob/master/03-%E4%BA%BA%E5%B7%A5%E6%99%BA%E6%85%A7/02-%E5%84%AA%E5%8C%96%E7%AE%97%E6%B3%95/01-%E5%82%B3%E7%B5%B1%E5%84%AA%E5%8C%96%E6%96%B9%E6%B3%95/01-%E5%84%AA%E5%8C%96/01-%E7%88%AC%E5%B1%B1%E6%BC%94%E7%AE%97%E6%B3%95/03-%E9%80%9A%E7%94%A8%E7%9A%84%E7%88%AC%E5%B1%B1%E6%A1%86%E6%9E%B6/tsp.py進行修改
+#本程式參考
+#https://blog.csdn.net/zw17302560727/article/details/122304577
+#和
+#https://github.com/ccc112b/py2cs/blob/master/03-%E4%BA%BA%E5%B7%A5%E6%99%BA%E6%85%A7/02-%E5%84%AA%E5%8C%96%E7%AE%97%E6%B3%95/01-%E5%82%B3%E7%B5%B1%E5%84%AA%E5%8C%96%E6%96%B9%E6%B3%95/01-%E5%84%AA%E5%8C%96/01-%E7%88%AC%E5%B1%B1%E6%BC%94%E7%AE%97%E6%B3%95/03-%E9%80%9A%E7%94%A8%E7%9A%84%E7%88%AC%E5%B1%B1%E6%A1%86%E6%9E%B6/tsp.py進行修改
 import random
 import copy
 import math
@@ -22,11 +25,11 @@ def distance(p1, p2): #計算兩點距離
     return ((x2-x1)**2+(y2-y1)**2)**0.5
 
 def distance_map():
-    map = [[0 for _ in range(path)] 
-				   for _ in range(path)]
-    for row in range(path):
-        for col in range(path):
-            map[row][col] = distance(path[row],path[col])
+    map = [[0 for _ in range(citys)] 
+				   for _ in range(citys)]
+    for row in range(citys):
+        for col in range(citys):
+            map[row][col] = distance(citys[row],citys[col])
         return map
 
 def pathLength(p): #計算總距離
@@ -34,14 +37,14 @@ def pathLength(p): #計算總距離
     plen = len(p)
     #print(plen)
     for i in range(1,plen):
-        dist += distance(citys[i-1], citys[i])
+        dist += distance(citys[p[i-1]], citys[p[i]])
         #print(dist)
     return dist
 
 def climb():
-    router = citys
-    length = len(citys)
-    distance = pathLength(citys)
+    router = path
+    length = len(path)
+    distance = pathLength(path)
 
     turn = 1000
     while turn:
